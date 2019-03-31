c359e2c090de0a4d6ef173b8b536945022760a86
Tae yoon <15tyc@queensu.ca>
2019-03-31 15:09:24 -0400

    Added editing and deleting listings functionality

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -10,56 +10,71 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String _emailValue;
-  String _passwordValue;
+  final Map<String, String> _formData = {'email': null, 'password': null};
+  final GlobalKey<FormState> _formkey = new GlobalKey<FormState>();
   bool _acceptTerms = false;
 
   DecorationImage _buildBackgroundImage() {
-    return  DecorationImage(
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5), BlendMode.dstATop),
-              image: AssetImage('assets/background.jpg'),
-            );
+    return DecorationImage(
+      fit: BoxFit.cover,
+      colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+      image: AssetImage('assets/background.jpg'),
+    );
   }
 
   Widget _buildEmailField() {
-    return TextField(
-                  decoration: InputDecoration(labelText: 'Email', filled: true, fillColor: Colors.white),
-                  onChanged: (String emailValue) {
-                    setState(() {
-                      _emailValue = emailValue;
-                    });
-                  },
-                );
+    return TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Email', filled: true, fillColor: Colors.white),
+        keyboardType: TextInputType.emailAddress,
+        validator: (String value) {
+          if (value.isEmpty ||
+              !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                  .hasMatch(value)) {
+            return 'Please input valid email';
+          }
+        },
+        onSaved: (String value) {
+          _formData['email'] = value;
+        });
   }
-  
+
   Widget _buildPasswordField() {
-    return TextField(
-                  decoration: InputDecoration(labelText: 'Password', filled: true, fillColor: Colors.white),
-                  obscureText: true,
-                  onChanged: (String passwordValue) {
-                    setState(() {
-                      _passwordValue = passwordValue;
-                    });
-                  },
-                );
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Password', filled: true, fillColor: Colors.white),
+      obscureText: true,
+      validator: (String value) {
+        if (value.trim().length < 8) {
+          return 'Password must be at least 8 characters long';
+        }
+      },
+      onSaved: (String value) {
+        _formData['password'] = value;
+      },
+    );
   }
 
   Widget _buildAcceptSwitch() {
     return SwitchListTile(
-                    value: _acceptTerms,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _acceptTerms = value;
-                      });
-                    },
-                    title: Text('Accept Terms'));
+        value: _acceptTerms,
+        onChanged: (bool value) {
+          setState(() {
+            _acceptTerms = value;
+          });
+        },
+        title: Text('Accept Terms'));
   }
 
   void _submitForm() {
-     Navigator.pushReplacementNamed(context, '/products');
+    if (!_formkey.currentState.validate()) {
+      return;
+    }
+    _formkey.currentState.save();
+    Navigator.pushReplacementNamed(context, '/products');
   }
+
   @override
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
@@ -69,27 +84,27 @@ class _AuthPageState extends State<AuthPage> {
           title: Text('Login'),
         ),
         body: Container(
-            decoration: BoxDecoration(
-                image: _buildBackgroundImage()),
+            decoration: BoxDecoration(image: _buildBackgroundImage()),
             padding: EdgeInsets.all(40.0),
             child: Center(
                 child: SingleChildScrollView(
                     child: Container(
-                      width: targetWidth,
-                      child: Column(
-              children: <Widget>[
-                _buildEmailField(),
-                SizedBox(height: 10.0),
-                _buildPasswordField(),
-                _buildAcceptSwitch(),
-                SizedBox(height: 10.0),
-                RaisedButton(
-                  child: Text('Login'),
-                  textColor: Colors.white,
-                  color: Theme.of(context).primaryColor,
-                  onPressed: _submitForm
-                )
-              ],
-            ))))));
+                        width: targetWidth,
+                        child: Form(
+                            key: _formkey,
+                            child: Column(
+                              children: <Widget>[
+                                _buildEmailField(),
+                                SizedBox(height: 10.0),
+                                _buildPasswordField(),
+                                _buildAcceptSwitch(),
+                                SizedBox(height: 10.0),
+                                RaisedButton(
+                                    child: Text('Login'),
+                                    textColor: Colors.white,
+                                    color: Theme.of(context).primaryColor,
+                                    onPressed: _submitForm)
+                              ],
+                            )))))));
   }
 }
