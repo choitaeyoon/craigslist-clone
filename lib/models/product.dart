cfcf29b8c41945bc09c06b04a5abd92c5f236ace
Tae yoon <15tyc@queensu.ca>
2019-03-31 20:51:02 -0400

    Implemented favorites feature

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -8,11 +8,13 @@ class Product {
   final double price;
   final String image;
   final String location;
+  final bool isFavorite;
 
   Product(
       {@required this.title,
       @required this.description,
       @required this.price,
       @required this.image,
-      @required this.location});
+      @required this.location,
+      @required this.isFavorite = false});
 }
